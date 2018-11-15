0512966e2345e249f79b3fddfc3134a9a1636506
Michael Lipscombe <mlipscombe@outlook.com>
2018-11-15 11:38:53 -0500

    removed quote

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -22,6 +22,6 @@ cp ./bin/dotnet-nuget-ext /usr/local/bin/
 dotnet pack-ext --version
 
 dotnet pack-ext simple ./src/TestApplication/TestApplication.csproj 
- --version-spec "1.0.0" --basepath ./src/TestApplication/publish --no-publish --no-build --source ./output . https://www.myget.org/F/dotnet-resource-test/api/v3/index.json`
-
-cp *.nupkg ../build-output
+ --version-spec "1.0.0" --basepath ./src/TestApplication/publish --no-publish --no-build --source ./output . https://www.myget.org/F/dotnet-resource-test/api/v3/index.json
+ 
+cp *.nupkg ./build-output
