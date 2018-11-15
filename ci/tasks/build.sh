03ce18d8d03deaa55d4ecace91dfbe230b534e38
Michael Lipscombe <mlipscombe@outlook.com>
2018-11-15 14:06:34 -0500

    add back auto increment

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-cd app-repo
-
-echo I am in `pwd`
-ls -l ../
 
 echo "starting build ..."
 
 dotnet restore -r ubuntu.14.04-x64
 dotnet publish -f netcoreapp2.1 -r ubuntu.14.04-x64 -o ./publish
 
-echo "copying files to ../build-output"
-cp manifest.yml ../build-output
+echo "downloading dotnet extensions..."
 
 wget -c https://github.com/miclip/dotnet-extensions/releases/download/v0.7/dotnet-ext-linux.tar.gz -O dotnet-ext-linux.tar.gz 
 tar -xvzf dotnet-ext-linux.tar.gz
@@ -20,9 +15,12 @@ cp ./bin/dotnet-pack-ext /usr/local/bin/
 cp ./bin/dotnet-nuget-ext /usr/local/bin/
 
 dotnet pack-ext --version
+
+echo "packing published output..."
  
-dotnet pack-ext simple ./src/TestApplication/TestApplication.csproj --basepath ./src/TestApplication/publish --no-publish --no-build --output $PWD --version "1.0.0" --source https://www.myget.org/F/dotnet-resource-test/api/v3/index.json
+dotnet pack-ext simple ./src/TestApplication/TestApplication.csproj --basepath ./src/TestApplication/publish --no-publish --no-build --output $PWD --version "1.0.*" --increment --source https://www.myget.org/F/dotnet-resource-test/api/v3/index.json
 
-cp *.nupkg ../build-output --verbose
 
+echo "copy package to build-output..."
+cp *.nupkg ../build-output
 ls ../build-output
