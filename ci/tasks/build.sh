46a5f394386f87c72fd1b86ce84a15184833f160
Michael Lipscombe <mlipscombe@outlook.com>
2018-11-15 13:53:47 -0500

    copy

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -21,8 +21,8 @@ cp ./bin/dotnet-nuget-ext /usr/local/bin/
 
 dotnet pack-ext --version
  
-dotnet pack-ext simple ./src/TestApplication/TestApplication.csproj --basepath ./src/TestApplication/publish --no-publish --no-build --output . --version "1.0.0" --source https://www.myget.org/F/dotnet-resource-test/api/v3/index.json
+dotnet pack-ext simple ./src/TestApplication/TestApplication.csproj --basepath ./src/TestApplication/publish --no-publish --no-build --output $PWD --version "1.0.0" --source https://www.myget.org/F/dotnet-resource-test/api/v3/index.json
 
-cp *.nupkg ./build-output
+cp ./*.nupkg ./build-output --verbose
 
 ls ./build-output
