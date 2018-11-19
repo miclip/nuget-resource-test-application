9e2db4242b9a4e542e7ba9c0f3cbf3c3a4ce72c4
Michael Lipscombe <mlipscombe@outlook.com>
2018-11-19 10:38:08 -0700

    fixes to pipeline

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -22,6 +22,6 @@ dotnet pack-ext simple ./src/TestApplication/TestApplication.csproj --basepath .
 
 
 echo "copy package to build-output..."
-mkdir ../build-output
+mkdir -p ../build-output
 cp *.nupkg ../build-output
 ls ../build-output
